cd73db0c8458985095b0c4016b54141e2c4ac9e1
qijunL <qijunl_lrving@163.com>
2019-11-27 15:38:47 +0800

    创建第一个web服务器

---
+++ webserver/node_js/server.js
@@ -0,0 +1,18 @@
+'use strict'
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+var express = require('express');
+var serveIndex = require('serve-index');
+var app = express();
+app.use(serveIndex('./public'));
+app.use(express.static('./public'));
+//http server
+var http_server = http.createServer(app);
+http_server.listen(8008,'0.0.0.0');
+
+//var options = {
+//}
+//https server
+//var https_server = https.createServer(options,app);
+//https_server.listen(443,'0.0.0.0')
